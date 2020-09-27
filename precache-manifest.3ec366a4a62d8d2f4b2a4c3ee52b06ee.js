e350fb9fada995d2db82c3859850c46b34c1b99b
LizandraBonfim <lizandra.oliveirasouza@hotmail.com>
2020-09-27 08:59:29 -0300

    Updates

---
+++ precache-manifest.3ec366a4a62d8d2f4b2a4c3ee52b06ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14188ae0095a79f3a927eb9108b1a43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "224f5f1f10f05f520924",
+    "url": "/static/css/main.ef8576be.chunk.css"
+  },
+  {
+    "revision": "2d4a921fcf0f1b71ed0c",
+    "url": "/static/js/2.23327e4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23327e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224f5f1f10f05f520924",
+    "url": "/static/js/main.2aeeed46.chunk.js"
+  },
+  {
+    "revision": "2bc6c2adad6d0dcdcd26",
+    "url": "/static/js/runtime-main.1821553b.js"
+  },
+  {
+    "revision": "7b02d20068225a43524d52476fb217f9",
+    "url": "/static/media/anna.7b02d200.jpg"
+  },
+  {
+    "revision": "75c5437cb13ca8f5b6805e11f875f265",
+    "url": "/static/media/elsa.75c5437c.jpg"
+  },
+  {
+    "revision": "c1a83e142d7bee1c9958c72c7dce3605",
+    "url": "/static/media/frozen-olaf.c1a83e14.jpg"
+  },
+  {
+    "revision": "db4ee51fc7409253aae34ed5e5fde08b",
+    "url": "/static/media/frozen.db4ee51f.jpg"
+  },
+  {
+    "revision": "f8ee55ea1d0bbe81bc286d268354e3d5",
+    "url": "/static/media/irmas.f8ee55ea.jpg"
+  },
+  {
+    "revision": "1197224039fbbd14e01af180eaa4f873",
+    "url": "/static/media/main.11972240.png"
+  },
+  {
+    "revision": "0b4db96a1a092c59c25a3241bb5f5229",
+    "url": "/static/media/todos.0b4db96a.jpg"
+  }
+]);
