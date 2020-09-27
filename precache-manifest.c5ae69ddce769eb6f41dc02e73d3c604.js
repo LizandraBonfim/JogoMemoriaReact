8e8a49ae2790a0c59c81840f86ebab8029f17b67
LizandraBonfim <lizandra.oliveirasouza@hotmail.com>
2020-09-27 10:00:36 -0300

    Updates

---
+++ precache-manifest.c5ae69ddce769eb6f41dc02e73d3c604.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f46306bf47dff844eb8102e4e071fd4",
+    "url": "/JogoMemoriaReact/index.html"
+  },
+  {
+    "revision": "224f5f1f10f05f520924",
+    "url": "/JogoMemoriaReact/static/css/main.ef8576be.chunk.css"
+  },
+  {
+    "revision": "2d4a921fcf0f1b71ed0c",
+    "url": "/JogoMemoriaReact/static/js/2.23327e4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JogoMemoriaReact/static/js/2.23327e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224f5f1f10f05f520924",
+    "url": "/JogoMemoriaReact/static/js/main.2aeeed46.chunk.js"
+  },
+  {
+    "revision": "bd245f9fa32265bc7fd0",
+    "url": "/JogoMemoriaReact/static/js/runtime-main.6a1232da.js"
+  },
+  {
+    "revision": "7b02d20068225a43524d52476fb217f9",
+    "url": "/JogoMemoriaReact/static/media/anna.7b02d200.jpg"
+  },
+  {
+    "revision": "75c5437cb13ca8f5b6805e11f875f265",
+    "url": "/JogoMemoriaReact/static/media/elsa.75c5437c.jpg"
+  },
+  {
+    "revision": "c1a83e142d7bee1c9958c72c7dce3605",
+    "url": "/JogoMemoriaReact/static/media/frozen-olaf.c1a83e14.jpg"
+  },
+  {
+    "revision": "db4ee51fc7409253aae34ed5e5fde08b",
+    "url": "/JogoMemoriaReact/static/media/frozen.db4ee51f.jpg"
+  },
+  {
+    "revision": "f8ee55ea1d0bbe81bc286d268354e3d5",
+    "url": "/JogoMemoriaReact/static/media/irmas.f8ee55ea.jpg"
+  },
+  {
+    "revision": "1197224039fbbd14e01af180eaa4f873",
+    "url": "/JogoMemoriaReact/static/media/main.11972240.png"
+  },
+  {
+    "revision": "0b4db96a1a092c59c25a3241bb5f5229",
+    "url": "/JogoMemoriaReact/static/media/todos.0b4db96a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ec366a4a62d8d2f4b2a4c3ee52b06ee.js"
+  "/JogoMemoriaReact/precache-manifest.c5ae69ddce769eb6f41dc02e73d3c604.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JogoMemoriaReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
